d292931b70d08627dd4b38ccd1b8337f3da64411
Rafael Ducasse <rafael.ducasse@fms-ea.com>
2025-11-13 14:18:53 +0100

    Creation de la classe Product

---
+++ src/aTGoodMarketPOOJava/Product.java
@@ -0,0 +1,59 @@
+package aTGoodMarketPOOJava;
+
+import java.time.LocalDate;
+
+public abstract class Product {
+	
+	private String name;
+	private double unitPrice;
+	private double stockQuantity;
+	private LocalDate pickingDate;
+	private int shelfLifeDays;
+	
+	public String getName() {
+		return name;
+	}
+	
+	public void setName(String name) {
+		this.name = name;
+	}
+	
+	public double getUnitPrice() {
+		return unitPrice;
+	}
+	
+	public void setUnitPrice(double unitPrice) {
+		this.unitPrice = unitPrice;
+	}
+	
+	public double getStockQuantity() {
+		return stockQuantity;
+	}
+	
+	public void setStockQuantity(double stockQuantity) {
+		this.stockQuantity = stockQuantity;
+	}
+	
+	public LocalDate getPickingDate() {
+		return pickingDate;
+	}
+	
+	public void setPickingDate(LocalDate pickingDate) {
+		this.pickingDate = pickingDate;
+	}
+	
+	public int getShelfLifeDays() {
+		return shelfLifeDays;
+	}
+	
+	public void setShelfLifeDays(int shelfLifeDays) {
+		this.shelfLifeDays = shelfLifeDays;
+	}
+	
+	public abstract LocalDate calculateExpirationDate();
+	
+	public static void updateStock() {
+		return;
+	}
+
+}
